f0040c99333b89f8c1b0b36faec74d6340e73d44
Sara <saragorleta@yahoo.com>
2021-03-17 15:39:42 +0100

    creato layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.app')
+
+@section ('title','pagina home')
+
+@section('content')
+<h1>La mia Home</h1>
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+    Eius molestias dolores minima obcaecati delectus atque aperiam corrupti facilis, 
+    qui doloribus, rerum sequi repudiandae error 
+    impedit vitae ducimus ullam cupiditate et!
+</p>
+@endsection
